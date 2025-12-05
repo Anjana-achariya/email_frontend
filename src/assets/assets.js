5e7d0b0e98a2ffa119f97a4024d1e2ae1d1c52a5
Anjana-achariya <131129025+Anjana-achariya@users.noreply.github.com>
2025-12-05 19:46:08 +0530

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,13 @@
+import logo from "./logo_full.png"
+import toggledark from "./settingsdark.png"
+import togglelight from "./settingslight.png"
+import emailIcon from "./email.png";
+import linkedinIcon from "./linkedin.png";
+import githubIcon from "./social.png";
+import hfIcon from "./hf.png";
+export const assets ={
+    logo,
+    toggledark,
+    togglelight,
+    emailIcon, linkedinIcon, githubIcon, hfIcon 
+}
